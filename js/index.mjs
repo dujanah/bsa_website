a030ef3d1b7c69350f73642eb3e95d121510b06c
Dujanah Zayn <dujanah.hasan@gmail.com>
2024-11-02 02:59:06 -0400

    modified front end

--- js/index.mjs
+++ js/index.mjs
@@ -1,13 +1,9 @@
-document.querySelector("#team-link").addEventListener("mouseenter", function() {
-    document.querySelector("#intro-image").style.height = "36vw";
-    document.querySelector("#intro-image").style.marginTop = "-4vw";
+document.querySelector("#team-nav").addEventListener("mouseenter", function() {
+    document.querySelector("#intro-image").style.height = "37vw";
+    document.querySelector("#intro-image").style.marginTop = "-5vw";
 });
 
-document.querySelector("#team-link").addEventListener("mouseleave", function() {
-    document.querySelector("#intro-image").style.height = "35vw";
+document.querySelector("#team-nav").addEventListener("mouseleave", function() {
     document.querySelector("#intro-image").style.marginTop = "-3vw";
-});
-
-document.querySelector("#team-link").addEventListener("mouseleave", function() {
     document.querySelector("#intro-image").style.height = ""; // Reset to original height
 });
